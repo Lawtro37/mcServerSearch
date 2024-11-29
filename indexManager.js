5cab5fe9221310704ec6b5a014bc666585627e7a
Lawtro37 <98205608+Lawtro37@users.noreply.github.com>
2024-11-29 22:03:53 +1000

    Update indexManager.js

--- indexManager.js
+++ indexManager.js
@@ -13,7 +13,6 @@ if(fs.existsSync('./masscan.bin')) {
     //make
 
     try{
-        execSync('apt-get --assume-yes install git make gcc');
         execSync('git clone https://github.com/robertdavidgraham/masscan')
         execSync('cd masscan');
         execSync('make');
