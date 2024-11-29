aa5b9b775f62ff44269747f0fb691cad8f78fa04
Lawtro37 <98205608+Lawtro37@users.noreply.github.com>
2024-11-29 22:11:36 +1000

    Update indexManager.js

--- indexManager.js
+++ indexManager.js
@@ -15,6 +15,7 @@ if(fs.existsSync('./masscan.bin')) {
     try{
         execSync('git clone https://github.com/robertdavidgraham/masscan')
         execSync('cd masscan');
+        execSync("Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))")
         execSync('choco install make')
         execSync('make');
 
